4bf1f202b4ec484025db12828b7919cfbac37823
Destiny onuiri <destinyonuiri@gmail.com>
2023-10-04 13:59:56 +0100

    start

--- router/router.ts
+++ router/router.ts
@@ -18,9 +18,9 @@ router.route("/create-account").post(createAccount);
 
 router.route("/sign-in-account").post(SignInUser);
 
-router.route("/:token/verify-account").get(firstAccountVerification);
+router.route("/:token/verify-account-start").get(firstAccountVerification);
 
-router.route("/:token/verify-account-start").get(accountVerification);
+router.route("/:token/verify-account").get(accountVerification);
 
 router.route("/all-account").get(allAccount);
 
